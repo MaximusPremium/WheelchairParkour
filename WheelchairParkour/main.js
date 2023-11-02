aea85fec8337859c9deb8e0dbdf91d6453ce8570
MaximusPremium <116764900+MaximusPremium@users.noreply.github.com>
2023-11-02 08:57:19 +0100

    Add files via upload

--- WheelchairParkour/main.js
+++ WheelchairParkour/main.js
@@ -5,7 +5,36 @@ const ctx = canv1.getContext("2d")
 const CW = canv1.width = 800
 const CH = canv1.height = 800
 
-let b1 = new Building(5,3,0)
+let b1 = new Building(12,8,0)
+let x = CW/2
+
 setInterval(()=>{
-    b1.draw(ctx,CH,CW/2)
+    ctx.clearRect(0,0,CW,CH)
+    b1.draw(ctx,CH,CW/2+x)
+    checkKeys()
 },0)
+
+let keyMap = {}
+let speed = 4
+
+document.addEventListener('keydown',(event)=>{
+    keyMap[event.key] = 1
+})
+
+document.addEventListener('keyup',(event)=>{
+    keyMap[event.key] = 0
+})
+
+function checkKeys(){
+    if(keyMap['w'] === 1 || keyMap['ArrowUp']) moveH(1)
+    if(keyMap['a'] === 1 || keyMap['ArrowLeft']) moveW(1)
+    if(keyMap['s'] === 1 || keyMap['ArrowDown']) moveH(-1)
+    if(keyMap['d'] === 1 || keyMap['ArrowRight']) moveW(-1)
+}
+
+function moveH(ver){
+    y += speed * ver 
+}
+function moveW(ver){
+    x += speed * ver 
+}
